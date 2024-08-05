1b6d996bac4eca213b092f662deb72d8c8db63eb
Your Name <saeed.ibrahim.abdulrahman@gmail.com>
2024-08-05 04:16:38 +0400

    feat: Add identifier utility functions

---
+++ src/utils/identifierUtils.js
@@ -0,0 +1,25 @@
+const componentTypeMap = {
+  'access_point': 'AP',
+  'ip_phone': 'IP',
+  'end_user_device': 'EU',
+  'switch': 'SW',
+  'router': 'RT',
+  'server': 'SR',
+  'firewall': 'FW',
+  'patch_panel': 'PP',
+  // Add more as needed
+};
+
+export const generateSmartIdentifier = (componentType, idfNumber, deviceNumber, portNumber) => {
+  const prefix = isEndUserDeviceType(componentType) ? 'A' : 'B';
+  const typeCode = componentTypeMap[componentType] || 'OT'; // OT for Other
+  const idf = idfNumber.toString().padStart(2, '0');
+  const device = deviceNumber.toString().padStart(2, '0');
+  const port = portNumber.toString().padStart(3, '0');
+
+  return `${prefix}${typeCode}-${idf}${device}-${port}`;
+};
+
+export const isEndUserDeviceType = (type) => {
+  return ['access_point', 'ip_phone', 'end_user_device'].includes(type);
+};
