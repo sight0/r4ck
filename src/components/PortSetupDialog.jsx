295741639bce78ca29750b4dbd017a84da1369dd
Your Name <saeed.ibrahim.abdulrahman@gmail.com>
2024-08-05 04:09:56 +0400

    feat: Add component type and number to PortSetupDialog

--- src/components/PortSetupDialog.jsx
+++ src/components/PortSetupDialog.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Select, MenuItem, FormControl, InputLabel, Box, Typography } from '@mui/material';
+import { generateSmartIdentifier, isEndUserDeviceType } from '../utils/identifierUtils';
 
-const PortSetupDialog = ({ open, onClose, ports, numIdfs, idf, onPortChange }) => {
+const PortSetupDialog = ({ open, onClose, ports, numIdfs, idf, onPortChange, componentType, componentNumber }) => {
+    const [localPorts, setLocalPorts] = useState(ports);
+
+    useEffect(() => {
+        setLocalPorts(ports);
+    }, [ports]);
+    const handleCableSourceChange = (index, value) => {
+        const updatedPorts = [...localPorts];
+        updatedPorts[index].cableSource = value;
+        updatedPorts[index].identifier = generateSmartIdentifier(
+            value,
+            idf,
+            componentNumber,
+            index + 1
+        );
+        setLocalPorts(updatedPorts);
+        onPortChange(index, 'cableSource', value);
+        onPortChange(index, 'identifier', updatedPorts[index].identifier);
+    };
+
     return (
-        <Dialog open={open} onClose={onClose} maxWidth="lg" fullWidth>
-            <DialogTitle>Port Setup</DialogTitle>
+        <Dialog open={open} onClose={() => onClose(localPorts)} maxWidth="lg" fullWidth>
+            <DialogTitle>Configure Ports for {componentType}</DialogTitle>
             <DialogContent>
                 <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))', gap: 2 }}>
-                    {ports.map((port, index) => (
+                    {localPorts.map((port, index) => (
                         <Box key={index} sx={{ border: '1px solid #ccc', padding: 1, borderRadius: 1 }}>
                             <TextField
                                 label={`Port ${index + 1}`}
                                 value={port.label || ''}
-                                onChange={(e) => onPortChange(index, 'label', e.target.value)}
+                                onChange={(e) => {
+                                    const updatedPorts = [...localPorts];
+                                    updatedPorts[index].label = e.target.value;
+                                    setLocalPorts(updatedPorts);
+                                    onPortChange(index, 'label', e.target.value);
+                                }}
                                 fullWidth
                                 margin="dense"
                             />
@@ -21,11 +46,16 @@ const PortSetupDialog = ({ open, onClose, ports, numIdfs, idf, onPortChange }) =
                                 <InputLabel>Device</InputLabel>
                                 <Select
                                     value={port.cableSource || ''}
-                                    onChange={(e) => onPortChange(index, 'cableSource', e.target.value)}
+                                    onChange={(e) => handleCableSourceChange(index, e.target.value)}
                                 >
                                     <MenuItem value="access_point">Access Point</MenuItem>
-                                    <MenuItem value="ip_telephone">IP Telephone</MenuItem>
+                                    <MenuItem value="ip_phone">IP Phone</MenuItem>
                                     <MenuItem value="end_user_device">End-User Device</MenuItem>
+                                    <MenuItem value="switch">Switch</MenuItem>
+                                    <MenuItem value="router">Router</MenuItem>
+                                    <MenuItem value="server">Server</MenuItem>
+                                    <MenuItem value="firewall">Firewall</MenuItem>
+                                    <MenuItem value="patch_panel">Patch Panel</MenuItem>
                                     {[...Array(numIdfs)].map((_, idx) => (
                                         idx + 1 !== idf && 
                                         <MenuItem key={idx + 1} value={`IDF_${idx + 1}`}>{`IDF ${idx + 1}`}</MenuItem>
@@ -34,6 +64,13 @@ const PortSetupDialog = ({ open, onClose, ports, numIdfs, idf, onPortChange }) =
                                     <MenuItem value="OTHER">Other</MenuItem>
                                 </Select>
                             </FormControl>
+                            <TextField
+                                label="Identifier"
+                                value={port.identifier || ''}
+                                InputProps={{ readOnly: true }}
+                                fullWidth
+                                margin="dense"
+                            />
                             {port.connectedTo && (
                                 <Typography variant="caption" display="block" sx={{ mt: 1 }}>
                                     Connected to: {port.connectedTo}
@@ -44,7 +81,7 @@ const PortSetupDialog = ({ open, onClose, ports, numIdfs, idf, onPortChange }) =
                 </Box>
             </DialogContent>
             <DialogActions>
-                <Button onClick={onClose}>Close</Button>
+                <Button onClick={() => onClose(localPorts)}>Close</Button>
             </DialogActions>
         </Dialog>
     );
@@ -57,6 +94,8 @@ PortSetupDialog.propTypes = {
     numIdfs: PropTypes.number.isRequired,
     idf: PropTypes.number.isRequired,
     onPortChange: PropTypes.func.isRequired,
+    componentType: PropTypes.string.isRequired,
+    componentNumber: PropTypes.number.isRequired,
 };
 
 export default PortSetupDialog;
