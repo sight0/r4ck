668d817e663bbad6db0e1327cfeae57540fc09fb
Your Name <saeed.ibrahim.abdulrahman@gmail.com>
2024-08-05 04:35:32 +0400

    feat: Add component sequence tracking in RackVisualization

--- src/components/RackVisualization.jsx
+++ src/components/RackVisualization.jsx
@@ -86,6 +86,19 @@ const RackVisualization = ({ currentIdf, setCurrentIdf, numIdfs, idfData, interI
         });
         setComponentSequences(sequences);
     }, [components]);
+    const [componentSequences, setComponentSequences] = useState({});
+
+    useEffect(() => {
+        const sequences = {};
+        components.forEach(comp => {
+            if (!sequences[comp.type]) {
+                sequences[comp.type] = 0;
+            }
+            sequences[comp.type]++;
+            comp.sequence = sequences[comp.type];
+        });
+        setComponentSequences(sequences);
+    }, [components]);
 
     const handleConnectionCreate = (newConnection) => {
         const deviceA = components.find(c => c.id === newConnection.deviceA.componentId);
