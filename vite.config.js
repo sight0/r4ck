7dac0aef536a9fa7f559f178bef65ca1b61efea0
Your Name <saeed.ibrahim.abdulrahman@gmail.com>
2024-08-16 06:45:16 +0400

    optimized vite config for build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the chunk size warning limit (in KB)
+    chunkSizeWarningLimit: 1000,
+
+    // Rollup options for manual chunking
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 })
