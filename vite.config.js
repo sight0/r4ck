c6c717288c71d770e0cdda2e50dac42be6087784
Your Name <saeed.ibrahim.abdulrahman@gmail.com>
2024-09-08 14:56:22 +0400

    vite.config base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/r4ck/',
+  base: '/r4ck/',
   plugins: [react()],
   build: {
     // Increase the chunk size warning limit (in KB)
